672f95aa6dd6318b55d77cba031301d1f79f5f68
Frederik Hahne <atomfrede@gmail.com>
2025-05-16 22:07:21 +0200

    update realtime content

--- src/main/java/com/github/atomfrede/jdconf_sample/RealtimeService.java
+++ src/main/java/com/github/atomfrede/jdconf_sample/RealtimeService.java
@@ -3,23 +3,25 @@ package com.github.atomfrede.jdconf_sample;
 import gg.jte.TemplateEngine;
 import gg.jte.TemplateOutput;
 import gg.jte.output.StringOutput;
-import org.springframework.beans.factory.annotation.Autowired;
+import net.datafaker.Faker;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.time.Instant;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Service
 public class RealtimeService {
 
-    @Autowired
-    private WebsocketHolder websocketHolder;
-    @Autowired
-    private TemplateEngine templateEngine;
+    private final Faker faker = new Faker();
+    private final WebsocketHolder websocketHolder;
+    private final TemplateEngine templateEngine;
+
+    public RealtimeService(WebsocketHolder websocketHolder, TemplateEngine templateEngine) {
+        this.websocketHolder = websocketHolder;
+        this.templateEngine = templateEngine;
+    }
 
     @Scheduled(fixedRate = 5000)
     public void updateNewsFeed(){
@@ -34,7 +36,7 @@ public class RealtimeService {
 
     private FeedItem randomFeedItem(){
 
-        return new FeedItem("Spring I/O", "This rocks", Instant.now());
+        return new FeedItem(faker.fallout().character(), faker.fallout().quote(), Instant.now());
     }
     public static class FeedItem {
         public String author;
